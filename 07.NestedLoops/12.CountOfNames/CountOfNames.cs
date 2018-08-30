5f60c7353c156a9c74599f075c0183648e7a9cf2
Gandjurov <Gandjurov@abv.bg>
2018-08-30 22:28:38 +0300

    Add Nested Loops

---
+++ 07.NestedLoops/12.CountOfNames/CountOfNames.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CountOfNames
+    {
+    static void Main()
+        {
+        string[] input = Console.ReadLine().Split(' ');
+        Array.Sort(input);
+
+        var words = new HashSet<string>();
+        for (int i = 0; i < input.Length; i++)
+            {
+            words.Add(input[i]);
+            }
+
+        int count = 0;
+        foreach (string letter in words)
+            {
+            for (int j = 0; j < input.Length; j++)
+                {
+                if (letter == input[j]) count++;
+                }
+
+            Console.WriteLine(letter + " -> " + count);
+            count = 0;
+            }
+        Console.WriteLine();
+        }
+    }
